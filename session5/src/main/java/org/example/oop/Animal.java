546f2a86a20e8445462cf733ef8c240a480a849a
Ehsanrezazadeh1 <ehsan.rzd@yahoo.com>
2024-01-07 20:38:28 +0330

    bank commit

---
+++ session5/src/main/java/org/example/oop/Animal.java
@@ -0,0 +1,9 @@
+package org.example.oop;
+
+public class Animal {
+
+    private String name;
+
+
+
+}
---
+++ session5/src/main/java/org/example/oop/Eating.java
@@ -0,0 +1,8 @@
+package org.example.oop;
+
+public interface Eating {
+
+    void eating();
+
+    void move();
+}
